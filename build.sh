bf50c80a8af450dde9e9d37d0fd1fec983e97c15
JadenJSJ <chahjadi@gmail.com>
2024-09-21 08:12:46 +0700

    Change build branch to dev/1.21.1

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 git clone https://github.com/PaperMC/Folia
+git checkout dev/1.21.1
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 cd Folia
